a7d18a40c227125772fee570118cb921c3afebd5
Bartek <bartlomiej.gedek@zgierz.edu.pl>
2021-03-09 16:58:15 +0100

    zz

---
+++ index2.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+const { request } = require('node:http');
+const { response } = require('express');
+
+const mysql = require('mysql');
+const DATABASE_HOST='localhost';
+const DATABASE_USER='admin';
+const DATABASE_PASSWORD='admin';
+const DATABASE_NAME='baza';
+
+const db = mysql.createConnection({
+    host: DATABASE_HOST,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME
+})
+
+
+db.connect((err) => {
+    if(err){
+        throw err
+    }
+    console.log('MySql connected')
+})
+
+
+app.get('/showEventById/<id>', (request, response) => {
+    let id = request.params.id
+    let sql = 'Select * from event where idEvent = ' +  mysql.escape(id)
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        response.send(response)
+    }) 
+})
+
+
+
+main().catch(console.error);
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000')
+})
